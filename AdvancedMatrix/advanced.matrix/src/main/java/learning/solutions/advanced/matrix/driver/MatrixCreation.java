4814ea1dca38b412ed6678525e2294403eeb7a54
Sanket <sanket.korgaonkar@gmail.com>
2017-04-19 20:51:40 -0500

    FEATURE:Enabled navigationEngine logging to file.

--- AdvancedMatrix/advanced.matrix/src/main/java/learning/solutions/advanced/matrix/driver/MatrixCreation.java
+++ AdvancedMatrix/advanced.matrix/src/main/java/learning/solutions/advanced/matrix/driver/MatrixCreation.java
@@ -4,6 +4,10 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Properties;
+
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.PatternLayout;
+
 import learning.solutions.advanced.matrix.domain.MatrixArchitect;
 import learning.solutions.advanced.matrix.engineeringLevel.NavigationEngine;
 
@@ -12,9 +16,18 @@ public class MatrixCreation {
 	private static Properties matrixConfig = null;
 
 	public static void main(String[] args) throws IOException {
+		configureLogging();
 		System.out.println("Hello to the Robo-Maze_World");
 		NavigationEngine navEngine = new NavigationEngine(getMatrixConfig());
-		MatrixArchitect architect = new MatrixArchitect(getMatrixConfig(), navEngine.getAnimationCalibratedRobotPath());
+		new MatrixArchitect(getMatrixConfig(), navEngine.getAnimationCalibratedRobotPath());
+	}
+
+	private static void configureLogging() {
+		FileAppender fa = new FileAppender();
+		fa.setFile("navEngineOutput/navEnginePath_" + Long.toString(System.currentTimeMillis()) + ".txt");
+		fa.setLayout(new PatternLayout(PatternLayout.DEFAULT_CONVERSION_PATTERN));
+		fa.activateOptions();
+		org.apache.log4j.Logger.getRootLogger().addAppender(fa);
 	}
 
 	public static Properties getMatrixConfig() throws IOException {
